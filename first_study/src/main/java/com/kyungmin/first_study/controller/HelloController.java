76f84f71d8dad9692d430f126bac380dd4408844
honor-sky <keung903@naver.com>
2022-07-20 09:56:52 +0900

    - controller, viewResolver가 어떻게 서로 동작하는지 공부한 내용을 주석 정리 - first build

--- first_study/src/main/java/com/kyungmin/first_study/controller/HelloController.java
+++ first_study/src/main/java/com/kyungmin/first_study/controller/HelloController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("HelloController")   // 웹 애플리케이션에서 /hello 라고 들어오면 이 메소드를 호출
-    public String hello(Model model){
+    @GetMapping("HelloController")   // 웹 애플리케이션 url이 /HelloController면 이 메소드를 호출하도록 맵핑
+    public String hello(Model model){ // spring boot에서 model 제공
         model.addAttribute("data","hello!!"); // Name 이라는 attr에 "hello!!"라는 값을 추가
         return "hello";
-
+        // controller가 viewResolver에  return 값인 hello와 model을 넘김
+        // viewResolver는 resource:templates:"hello" 를 찾아 템플릿 엔진 처리 후 웹브라우저에 변환된 html이 그려짐
     }
 
 }
